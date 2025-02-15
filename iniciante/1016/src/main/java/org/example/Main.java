1273af167cc3e0c834f3fd0cd2658bc24e21ddc2
Taw-Ham Almeida Balbino de Paula <taw.ham.balbino9914@gmail.com>
2025-02-15 10:53:02 -0300

    problema 1016 resolvido

---
+++ iniciante/1016/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int distance = scanner.nextInt();
+        int result = distance * 2;
+
+        System.out.println(result + " minutos");
+
+    }
+}
