9ef68e17f673306b79d30491d59c5ca2776db201
tawhamjavascript <taw.ham.balbino9914@gmail.com>
2025-02-26 09:08:55 -0300

    problema 1041 resolvido

---
+++ iniciante/1041/src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+
+        if (x == 0 && y == 0) {
+            System.out.println("Origem");
+        }
+        else if (x == 0 && y != 0) {
+            System.out.println("Eixo Y");
+        }
+        else if (x != 0 && y == 0) {
+            System.out.println("Eixo X");
+        }
+
+        else if (x > 0) {
+            if (y > 0) {
+                System.out.println("Q1");
+            }
+            else {
+                System.out.println("Q4");
+            }
+        }
+        else if (x < 0) {
+            if (y > 0) {
+                System.out.println("Q2");
+            }
+            else {
+                System.out.println("Q3");
+            }
+        }
+    }
+}
